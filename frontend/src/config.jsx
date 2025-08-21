ecf98a593b7f65b633d29a9923adcd47827fb9e0
AhmadIqbal246 <ahmadiqbalhsp@gmail.com>
2025-08-21 21:25:54 +0500

    Add Celery for task management and update .gitignore for Django and Vite React
    - Added Celery and its Redis dependency to requirements for asynchronous task processing.
    - Updated .gitignore to include common Django and Vite React files and directories to prevent unnecessary files from being tracked.
    - Refactored frontend configuration to use environment variables for better flexibility and security.

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,7 +1,7 @@
 const ENV = {
-  GOOGLE_OAUTH2_CLIENT_ID: "228111222833-rsj5r10b71luierri8vh8fbdfmebupdu.apps.googleusercontent.com",
-  BASE_APP_URL: "http://localhost:5173",
-  BASE_API_URL: "http://localhost:8000",
+  GOOGLE_OAUTH2_CLIENT_ID: import.meta.env.VITE_GOOGLE_OAUTH2_CLIENT_ID,
+  BASE_APP_URL: import.meta.env.VITE_BASE_APP_URL,
+  BASE_API_URL: import.meta.env.VITE_BASE_API_URL,
 };
 
 export default ENV;
